d771a907eaf56bb57e21d851d3581b9a64507e65
IT Student <Chandler Taylor>
2018-03-05 22:24:45 -0700

    Marth 5th Update

--- db_setup.sql
+++ db_setup.sql
@@ -1,10 +1,9 @@
 /*
-* Chandler Taylor SQL Model 2-20-18
+* 	Chandler Taylor 
+*	SQL Model 
+*	March 5 2018
 * 
-* TODO: Still need to finish FK's for 1-1 relationships and review
-* the syntax for the 1-M relationships already implemented below
-* http://www.tech-recipes.com/rx/56738/one-to-one-one-to-many-table-relationships-in-sql-server/
-*
+* 
 * ATTENTION: On some of the online MySQL workbenchs FK's werent working on 
 * TABLE CREATE so I had to use:
 * 
@@ -13,7 +12,14 @@
 * TO add the FK's after the tables were created. 
 * REFERENCES In Product subclasses may not need reference to 
 * Product ID. Just enforce it manually in your DB.
+*
+* Look at this link below and finish up the products and order/orderitems tables so they
+* are in 3NF and are working and referencing ID's how they should be
+* https://www.mikesdotnetting.com/article/210/razor-web-pages-e-commerce-adding-a-shopping-cart-to-the-bakery-template-site
 */
+
+CREATE DATABASE bluehalo;
+USE bluehalo;
 
 CREATE TABLE customer (
 	cust_id INT AUTO_INCREMENT NOT NULL,
@@ -24,71 +30,98 @@ CREATE TABLE customer (
 	PRIMARY KEY (cust_id)
 );
 
-CREATE TABLE cart (
+CREATE TABLE order (
 	cart_id INT AUTO_INCREMENT NOT NULL,
+	customer_id NOT NULL,
 	status VARCHAR(64) NOT NULL,
-	Fk_product_id INT,
-	PRIMARY KEY (cart_id),
+	total_cost MONEY,
+	date_created DATE NOT NULL,
+	checked_out BIT NOT NULL DEFAULT 0,
+	PRIMARY KEY (cart_id)
 	/*FOREIGN KEY (Fk_product_id) REFERENCES products(product_id)*/
 );
 
+CREATE TABLE orderitems {
+	id INT AUTO_INCREMENT NOT NULL,
+	product_id
+};
+
 CREATE TABLE employee (
 	employee_id INT AUTO_INCREMENT NOT NULL,
-	Fk_cart_id INT,
-	PRIMARY KEY (employee_id),
+	employee_name VARCHAR(40) NOT NULL,
+	logged_in BOOLEAN,
+	administrator BOOLEAN NOT NULL,
+	password VARCHAR(40),
+	PRIMARY KEY (employee_id)
 	/*FOREIGN KEY (Fk_cart_id) REFERENCES cart(cart_id)*/
 );
 
-CREATE TABLE products (
+/*CREATE TABLE products (
 	product_id INT AUTO_INCREMENT NOT NULL,
-	quantity_in_stock INT NOT NULL,
-	price INT NOT NULL,
+	inventory INT NOT NULL,
+	price DECIMAL NOT NULL,
+	image_path VARCHAR(255),
 	Fk_review_id INT,
-	PRIMARY KEY (product_id),
-	/*FOREIGN KEY (Fk_review_id) REFERENCES review(review_id)*/ 
-);
+	PRIMARY KEY (product_id)
+);*/
 
 CREATE TABLE review (
-	review_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	review_id INT NOT NULL,
 	review_date TIMESTAMP,
 	rating INT NOT NULL,
 	review_text MEDIUMTEXT NOT NULL,
-	author VARCHAR(255)
+	author VARCHAR(255),
+	PRIMARY KEY (review_id)
 );
 
-/*
-	Probably don't need the REFERENCES on lines, reeels, rods, or flies. 
-	Just manually enforce it. Lines has already been changed to 
-	reflect these changes. Make sure it works before chanigng everything.
-*/
-
-CREATE TABLE lines (
-	product_id INT PRIMARY KEY NOT NULL,
+CREATE TABLE flylines (
+	id INT AUTO_INCREMENT NOT NULL,
 	weight VARCHAR(255) NOT NULL,
 	sink_rate VARCHAR(64) NOT NULL,
 	brand VARCHAR(64) NOT NULL,
-	model VARCHAR(64) NOT NULL
+	model VARCHAR(64) NOT NULL,
+	inventory INT NOT NULL,
+	price MONEY NOT NULL,
+	image_path VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE reels (
-	reel_id INT PRIMARY KEY NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	weight VARCHAR(64) NOT NULL,
 	brand VARCHAR(64) NOT NULL,
 	model VARCHAR(64) NOT NULL,
-	color VARCHAR(64) NOT NULL
+	color VARCHAR(64) NOT NULL,
+	inventory INT NOT NULL,
+	price DECIMAL NOT NULL,
+	image_path VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE rods (
-	rod_id INT PRIMARY KEY NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	weight VARCHAR(64) NOT NULL,
 	brand VARCHAR(64) NOT NULL,
 	model VARCHAR(64) NOT NULL,
-	style VARCHAR(64) NOT NULL
+	style VARCHAR(64) NOT NULL,	
+	inventory INT NOT NULL,
+	price DECIMAL NOT NULL,
+	image_path VARCHAR(255),
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE flies (
-	flies_id INT PRIMARY KEY NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
 	name VARCHAR(64) NOT NULL,
 	color VARCHAR(64) NOT NULL,
-	fly_size INT NOT NULL
+	fly_size INT NOT NULL,
+	inventory INT NOT NULL,
+	price DECIMAL NOT NULL,
+	image_path VARCHAR(255),
+	PRIMARY KEY (id)
 );
+
+ALTER TABLE flylines AUTO_INCREMENT=1000;
+ALTER TABLE reels AUTO_INCREMENT=2000;
+ALTER TABLE rods AUTO_INCREMENT=3000;
+ALTER TABLE flies AUTO_INCREMENT=4000;
